71c1183e0fe14d315abf1b7c69be9d072ad51d25
BrunoJScript <shootingworldglockpt@gmail.com>
2025-06-08 00:14:52 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const botaoFiltrar = document.querySelector(`.btn-filtrar`);
+botaoFiltrar.addEventListener(`click`, function () {
+  //console.log("clicou no botao filtrar");
+  const categoriaSelecionada = document.querySelector(`#categoria`);
+  console.log(`categoriaSelecionada`);
+});
